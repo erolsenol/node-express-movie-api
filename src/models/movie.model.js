25fecb71f08a0691e47e15f21674423661ee1cae
erolsenol <erol.senol@outlook.com>
2023-05-14 16:36:11 +0300

    Project Movie Server Init

---
+++ src/models/movie.model.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose')
+const { toJSON, paginate } = require('./plugins')
+
+const movieSchema = mongoose.Schema(
+  {
+    plot: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fullplot: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    genres: {
+      type: Array,
+    },
+    runtime: {
+      type: Number,
+    },
+    cast: {
+      type: Array,
+    },
+    num_comments: {
+      type: Number,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    countries: {
+      type: Array,
+    },
+    released: {
+      type: Date,
+    },
+    directors: {
+      type: Array,
+    },
+    rated: {
+      type: String,
+    },
+    awards: {
+      type: Object,
+    },
+    lastupdated: {
+      type: String,
+    },
+    year: {
+      type: String,
+    },
+    imdb: {
+      type: Object,
+    },
+    type: {
+      type: String,
+    },
+    tomatoes: {
+      type: Object,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// add plugin that converts mongoose to json
+movieSchema.plugin(toJSON)
+movieSchema.plugin(paginate)
+
+/**
+ * @typedef Movie
+ */
+const Movie = mongoose.model('Movie', movieSchema)
+
+module.exports = Movie
---
+++ src/validations/movie.validation.js
@@ -0,0 +1,106 @@
+const Joi = require('joi')
+const { objectId } = require('./custom.validation')
+
+const createMovie = {
+  body: Joi.object().keys({
+    plot: Joi.string(),
+    fullplot: Joi.string(),
+    genres: Joi.array().items(Joi.string()),
+    runtime: Joi.number(),
+    cast: Joi.array().items(Joi.string()),
+    num_comments: Joi.number(),
+    title: Joi.string(),
+    countries: Joi.array().items(Joi.string()),
+    released: Joi.date(),
+    directors: Joi.array().items(Joi.string()),
+    rated: Joi.string(),
+    awards: Joi.object({
+      wins: Joi.number(),
+      nominations: Joi.number(),
+      text: Joi.string(),
+    }),
+    lastupdated: Joi.string(),
+    year: Joi.number(),
+    type: Joi.string(),
+    imdb: Joi.object({
+      rating: Joi.number(),
+      votes: Joi.number(),
+      id: Joi.number(),
+    }),
+    tomatoes: Joi.object({
+      rating: Joi.number(),
+      numReviews: Joi.number(),
+      meter: Joi.number(),
+      lastUpdated: Joi.date(),
+    }),
+  }),
+}
+
+const getMovies = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+}
+
+const getMovie = {
+  params: Joi.object().keys({
+    movieId: Joi.string().custom(objectId),
+  }),
+}
+
+const updateMovie = {
+  params: Joi.object().keys({
+    movieId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      plot: Joi.string(),
+      fullplot: Joi.string(),
+      genres: Joi.array().items(Joi.string()),
+      runtime: Joi.number(),
+      cast: Joi.array().items(Joi.string()),
+      num_comments: Joi.number(),
+      title: Joi.string(),
+      countries: Joi.array().items(Joi.string()),
+      released: Joi.date(),
+      directors: Joi.array().items(Joi.string()),
+      rated: Joi.string(),
+      awards: Joi.object({
+        wins: Joi.number(),
+        nominations: Joi.number(),
+        text: Joi.string(),
+      }),
+      lastupdated: Joi.string(),
+      year: Joi.number(),
+      type: Joi.string(),
+      imdb: Joi.object({
+        rating: Joi.number(),
+        votes: Joi.number(),
+        id: Joi.number(),
+      }),
+      tomatoes: Joi.object({
+        rating: Joi.number(),
+        numReviews: Joi.number(),
+        meter: Joi.number(),
+        lastUpdated: Joi.date(),
+      }),
+    })
+    .min(1),
+}
+
+const deleteMovie = {
+  params: Joi.object().keys({
+    movieId: Joi.string().custom(objectId),
+  }),
+}
+
+module.exports = {
+  createMovie,
+  getMovies,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+}
